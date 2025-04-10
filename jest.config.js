0112028c5d7823835e8984a4f3da4d10f40764de
Eduard Fabian Baron <edwardbaron99@gmail.com>
2025-04-10 00:15:23 -0500

    Use text regex

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testMatch: [
-    '<rootDir>/src/**/*.spec.ts',
-    '<rootDir>/src/**/*.spec.tsx',
-    '<rootDir>/src/**/*.test.ts',
-    '<rootDir>/src/**/*.test.tsx',
-  ],
+  testRegex: 'src/.*\\.(test|spec)\\.(ts|tsx)$',
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
