1d9d616ae4c425f5bf86fff6da2c3ea3c0f0ef0c
Eduard Fabian Baron <edwardbaron99@gmail.com>
2025-04-10 01:13:09 -0500

    Move file to tests folder

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
   testRegex: 'tests/.*\\.(test|spec)\\.(ts|tsx)$',
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
